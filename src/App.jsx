2eca081ad1da4acbfef2df609b0a6503f5c1363c
Adolfo-Contreras <adolfo.alvaradocontreras1760@stu.mtec.edu>
2024-04-25 09:19:17 -0600

    added a theme and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 import Typography from '@mui/material/Typography'
+import Navbar from './components/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Typography variant="h1" color="initial">Welcome to No-Sense News App</Typography>
+    <Navbar/>
+      <Typography variant="h3" color="initial">Welcome to No-Sense News App</Typography>
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,15 @@
+import { Box, AppBar, Toolbar, Typography } from "@mui/material";
+
 export default function Navbar() {
-    
+    return (
+        <Box>
+            <AppBar position="fixed" color="error">
+              <Toolbar>
+                <Typography variant="h6">
+                  <span>No/Sense</span> News
+                </Typography>
+              </Toolbar>
+            </AppBar>
+        </Box>
+    )
 }
