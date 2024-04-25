33c4b316c6dd47d50b5617be29665d70fc4786e6
Adolfo-Contreras <adolfo.alvaradocontreras1760@stu.mtec.edu>
2024-04-25 09:06:50 -0600

    added keen slider and also added a title to homepage

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Typography from '@mui/material/Typography'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Typography variant="h1" color="initial">Welcome to No-Sense News App</Typography>
     </>
   )
 }
