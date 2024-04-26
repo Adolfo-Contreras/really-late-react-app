e4e788ff070fe433801f9e5bc11def8096d73b06
Adolfo-Contreras <adolfo.alvaradocontreras1760@stu.mtec.edu>
2024-04-26 09:42:35 -0600

    added props to the news card

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,16 +1,16 @@
-import { useRef, useState, useEffect,  } from 'react';
 import 'keen-slider/keen-slider.min.css'
 import { useKeenSlider } from 'keen-slider/react.es';
 import NewsCard from './NewsCard';
 
+const thingy = {title:'jimmy did a big oopsie', description:'he killed someone'}
 export default function Carousel() {
   const [sliderRef] = useKeenSlider({slideChanged(){console.log('slide changed')}, loop:true, mode:'free-snap', slides:{perView:3, spacing: 10}}); 
   return (
     <div ref={sliderRef} className='keen-slider'>
-      <div className='keen-slider__slide'><NewsCard props={{title:'ligma', description:'description'}}/></div>
-      <div className='keen-slider__slide'><NewsCard/></div>
-      <div className='keen-slider__slide'><NewsCard/></div>
-      <div className='keen-slider__slide'><NewsCard/></div>
+      <div className='keen-slider__slide'><NewsCard data={thingy} /></div>
+      <div className='keen-slider__slide'><NewsCard data={thingy}/></div>
+      <div className='keen-slider__slide'><NewsCard data={thingy}/></div>
+      <div className='keen-slider__slide'><NewsCard data={thingy}/></div>
     </div>
   );
 }
--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -1,12 +1,12 @@
 import { Card, CardActions, CardContent,  Link,  Typography } from "@mui/material";
 
-export default function NewsCard(props) {
-    const {title, description} = props
+export default function NewsCard({data}) {
+    console.log(data)
     return (
         <Card>
             <CardContent>
-                <Typography variant="h4" color="initial">{title}</Typography>
-                <Typography variant="body2" color="initial">Amet sit do id in laborum pariatur nostrud id anim velit nostrud ullamco aute. Excepteur veniam sit reprehenderit deserunt ullamco elit enim adipisicing ut velit duis. Aute ea consequat Lorem aute ad dolore officia dolore fugiat adipisicing culpa incididunt et mollit. Reprehenderit sit est do Lorem cupidatat sit anim minim laboris commodo reprehenderit nisi. Aute officia proident laboris adipisicing in. Ea sit in qui aute est occaecat exercitation ut cillum amet consequat. Labore consectetur qui consectetur nostrud enim cupidatat sunt dolore anim et elit culpa id.</Typography>
+                <Typography variant="h4" color="initial">{data.title}</Typography>
+                <Typography className=" px-3" variant="body2" color="initial">{data.description}</Typography>
             </CardContent>
             <CardActions>
                 <Link>Read More</Link>
